caadf6a8ec1b7f99408f506a7774fb3dac6a216f
Eduardo Vieira <eduardo.vieira@maxmilhas.com.br>
2023-10-27 14:39:37 -0300

    apis only get methods

---
+++ HFApp.WEB/Controllers/FileEntitiesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HFApp.WEB.Data;
+using HFApp.WEB.Models.Domain.Entities;
+
+namespace HFApp.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileEntitiesController : ControllerBase
+    {
+        private readonly HFDbContext _context;
+
+        public FileEntitiesController(HFDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FileEntities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FileEntity>>> GetFileEntities()
+        {
+          if (_context.FileEntities == null)
+          {
+              return NotFound();
+          }
+            return await _context.FileEntities.ToListAsync();
+        }
+
+        // GET: api/FileEntities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FileEntity>> GetFileEntity(int? id)
+        {
+          if (_context.FileEntities == null)
+          {
+              return NotFound();
+          }
+            var fileEntity = await _context.FileEntities.FindAsync(id);
+
+            if (fileEntity == null)
+            {
+                return NotFound();
+            }
+
+            return fileEntity;
+        }
+    }
+}
---
+++ HFApp.WEB/Controllers/UserEntitiesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HFApp.WEB.Data;
+using HFApp.WEB.Models.Domain.Entities;
+
+namespace HFApp.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserEntitiesController : ControllerBase
+    {
+        private readonly HFDbContext _context;
+
+        public UserEntitiesController(HFDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserEntities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUserEntities()
+        {
+          if (_context.UserEntities == null)
+          {
+              return NotFound();
+          }
+            return await _context.UserEntities.ToListAsync();
+        }
+
+        // GET: api/UserEntities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserEntity>> GetUserEntity(int? id)
+        {
+          if (_context.UserEntities == null)
+          {
+              return NotFound();
+          }
+            var userEntity = await _context.UserEntities.FindAsync(id);
+
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return userEntity;
+        }
+    }
+}
